4f2674d0b26a203a2074cdb6f864f8f675ebcbfb
Yunus AbdulMajid <Yunusabdulmajidyunus38@gmail.com>
2024-08-25 11:49:03 +0100

    repo setup and nav component

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import NavigationBar from "./components/nav-bar";
+
+export default function App() {
+  return (
+    <div className="">
+      <NavigationBar navStyle="bg-gray-500"/>
+      <p className="mt-24">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque,
+        explicabo. Nesciunt consequuntur numquam veritatis officia animi illum
+        ipsam velit eos molestias. Ut quia, inventore soluta praesentium quos
+        voluptatem repellendus dolorum error aperiam dicta, quibusdam neque
+        aspernatur velit quaerat eveniet impedit nostrum voluptatibus quo!
+        Blanditiis quidem expedita excepturi molestiae quo corrupti corporis
+        reiciendis possimus facere. Blanditiis reiciendis tenetur laudantium vel
+        magni, harum at nihil saepe ab sit incidunt ducimus, ea, pariatur cumque
+        beatae accusamus dolore! Consequatur enim saepe quod, dolores officiis
+        facilis officia quae itaque minima laudantium est consequuntur pariatur
+        totam repellendus quisquam reprehenderit ipsum ducimus, et excepturi
+        dicta maxime. Molestiae.
+      </p>
+    </div>
+    
+  );
+}
